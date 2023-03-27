1f8db1f1ac225c2671ce9b33dac3f4ff1f326c10
Anusuya <anusuyabalakrishnan01@gmail.com>
2023-03-27 15:26:05 +0530

    database

--- gmail/databaseConnection.py
+++ gmail/databaseConnection.py
@@ -1,7 +1,7 @@
 import pymongo
 
 # client = pymongo.MongoClient("mongodb://localhost:27017")
-client = pymongo.MongoClient("mongodb+srv://root:root@cluster0.rhwqd.mongodb.net/?retryWrites=true&w=majority")
+client = pymongo.MongoClient("mongodb+srv://root:root@cluster0.rhwqd.mongodb.net/?retryWrites=true&w=majority",connectTimeoutMS=30000, socketTimeoutMS=None, socketKeepAlive=True, connect=False, maxPoolsize=1)
 
 db = client.LoginDatabase
 # db=client["LoginDatabase"]
